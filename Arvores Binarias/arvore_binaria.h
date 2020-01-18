ae31677d111cdef2926069aad7e61c4ad56778ee
fredsonchaves07 <fredsonchaves07@gmail.com>
2020-01-18 15:18:22 -0300

    Operacoes das arvores binarias, pecurso

--- Arvores Binarias/arvore_binaria.h
+++ Arvores Binarias/arvore_binaria.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define fmt "%d\n"
 
 typedef struct dados Dados;
 
@@ -9,11 +10,52 @@ typedef struct arvore{
     struct arvore *dir;
 } *Arvorebin;
 
-Arvorebin arvore(Arvorebin esq, Arvorebin dir, Dados dados){
+Arvorebin arvore(Arvorebin esq, Dados dados, Arvorebin dir){
     Arvorebin A = malloc(sizeof(struct arvore));
     A->esq = esq;
     A->dir = dir;
     A->dados = dados;
 
     return A;
+}
+
+void pecurso_ordem(Arvorebin A){
+    if(A == NULL){
+        return;
+    }
+
+    pecurso_ordem(A->esq);
+    printf(fmt, A->dados);
+    pecurso_ordem(A->dir);
+}
+
+void pecurso_preordem(Arvorebin A){
+    if(A == NULL){
+        return;
+    }
+
+    printf(fmt, A->dados);
+    pecurso_preordem(A->esq);
+    pecurso_ordem(A->dir);
+}
+
+void pecurso_posordem(Arvorebin A){
+    if(A == NULL){
+        return;
+    }
+
+    pecurso_posordem(A->esq);
+    pecurso_posordem(A->dir);
+    printf(fmt, A->dados);
+}
+
+void destroi(Arvorebin *A){
+    if(*A == NULL){
+        return;
+    }
+
+    destroi(&(*A)->esq);
+    destroi(&(*A)->dir);
+    free(*A);
+    *A = NULL;
 }
--- Arvores Binarias/program.c
+++ Arvores Binarias/program.c
@@ -10,7 +10,23 @@ struct dados{
 Dados dados1;
 
 int main(){
+    dados1.codigo = 4;
+    Arvorebin A4 = arvore(NULL, dados1, NULL);
+    dados1.codigo = 5;
+    Arvorebin A5 = arvore(NULL, dados1, NULL);
+    dados1.codigo = 6;
+    Arvorebin A6 = arvore(NULL, dados1, NULL);
+    dados1.codigo = 3;
+    Arvorebin A3 = arvore(NULL, dados1, A6);
+    dados1.codigo = 2;
+    Arvorebin A2 = arvore(A4, dados1, A5);
     dados1.codigo = 1;
-    Arvorebin A = arvore(NULL, NULL, dados1);
+    Arvorebin A1 = arvore(A2, dados1, A3);
     
+    printf("Pecorrendo dados em Ordem\n");
+    pecurso_ordem(A1);
+    printf("\nPecorrendo dados em Pré Ordem\n");
+    pecurso_preordem(A1);
+    printf("\nPecorrendo dados em Pós Ordem\n");
+    pecurso_posordem(A1);
 }
